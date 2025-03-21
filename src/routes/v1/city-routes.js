5a96bacdf0bd3102075641443b5a96b096eefd92
Kushagra Anand <kushagranand12345@gmail.com>
2025-03-21 20:32:01 +0530

    Initial commit with project files

---
+++ src/routes/v1/city-routes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const { CityController } = require('../../controllers');
+const { CityMiddlewares } = require('../../middlewares');
+
+const router = express.Router();
+
+// /api/v1/cities POST
+router.post('/', 
+        CityMiddlewares.validateCreateRequest,
+        CityController.createCity);
+
+// /api/v1/cities/:id DELETE
+router.delete('/:id', 
+        CityController.destroyCity);
+
+
+module.exports = router;
